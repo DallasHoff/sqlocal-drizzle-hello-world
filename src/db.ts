7ed19dd0c0450a326f0cfe21746b3527f1f64085
Dallas Hoffman <dallashoff@gmail.com>
2024-12-31 16:01:21 -0500

    Update dependencies; add batchDriver

--- src/db.ts
+++ src/db.ts
@@ -2,6 +2,6 @@ import { SQLocalDrizzle } from 'sqlocal/drizzle';
 import { drizzle } from 'drizzle-orm/sqlite-proxy';
 
 // Initialize Drizzle with SQLocal driver
-const { driver } = new SQLocalDrizzle('database.sqlite3');
-export const db = drizzle(driver);
+const { driver, batchDriver } = new SQLocalDrizzle('database.sqlite3');
+export const db = drizzle(driver, batchDriver);
 
